cb32f5350bad752108762eea0b6f586b9a67bc14
biglavis <lavis.chen5@gmail.com>
2024-08-10 22:25:36 -0700

    Added RiotCog for LoL player stats.

---
+++ cogs/riot.py
@@ -0,0 +1,132 @@
+import re
+import json
+from datetime import timedelta
+import modules.riot_tools as riot
+
+import discord
+from discord.ext import commands
+
+JSON_PATH = "json//summoners.json"
+
+class RiotCog(commands.Cog):
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+        # load accounts
+        try:
+            with open(JSON_PATH, 'r') as f:
+                self.db = json.load(f)
+        except FileNotFoundError:
+            self.db = {}
+
+        print(f"cog: {self.qualified_name} loaded")
+
+    @commands.command()
+    async def register(self, ctx: commands.Context, *, riotId: str):
+        riotId = riotId.split("#")
+
+        if len(riotId) != 2:
+            await error(ctx, "Invalid Riot ID.\nRiot ID must be in the form {gameName}#{tagLine}.")
+            return
+
+        if not (summoner := riot.get_summoner_by_name(riotId[0], riotId[1])):
+            await error(ctx, "Account not found.")
+            return
+
+        self.db[str(ctx.author.id)] = summoner
+
+        # save account
+        with open(JSON_PATH, 'w') as f:
+            json.dump(self.db, f, indent=4, default=str)
+
+        await self.summoner(ctx)
+
+    @commands.command()
+    async def summoner(self, ctx: commands.Context, *, user: str = None):
+        if not (tupl := await self.get_summoner(ctx, user)):
+            return
+        
+        summoner, member = tupl
+
+        embed = discord.Embed(title=f"**{summoner['gameName']}#{summoner['tagLine']}**", description=f"**Level: **{summoner['summonerLevel']}")
+        embed.set_thumbnail(url=riot.get_summoner_icon(summoner['profileIconId']))
+        if member:
+            embed.set_author(name=member.display_name, icon_url=member.display_avatar)
+
+        if stats := riot.get_stats_by_summoner(summoner['id']):
+            for entry in [entry for entry in stats if "leagueId" in entry]:
+                embed.add_field(name=" ".join(entry['queueType'].split("_")[:-1]).title(), value=f"{entry['tier'].title()} {entry['rank']} `({entry['wins']}W|{entry['losses']}L)`")
+        
+        if matchId := riot.get_matchId_by_puuid(summoner['puuid'], 3):
+            def win_str(win: bool):
+                if win: return "W"
+                return "L"
+            
+            def time_str(duration: int):
+                return f"{str(timedelta(seconds=duration)).lstrip(':0')}"
+            
+            def pos_str(position: str):
+                if not position: return ""
+                if position == "UTILITY": return " (Support)"
+                return f" ({position.title()})"
+            
+            matches = list(filter(None, [riot.get_match_by_matchId(id) for id in matchId]))
+            value = ""
+            for matchDto in matches:
+                infoDto = matchDto['info']
+                participantDto = [participantDto for participantDto in infoDto['participants'] if participantDto['puuid'] == summoner['puuid']][0]
+                teamDto = [teamDto for teamDto in infoDto['teams'] if teamDto['teamId'] == participantDto['teamId']][0]
+
+                value += f"<t:{str(infoDto['gameCreation'] + infoDto['gameDuration']*1000)[:-3]}:R>\n"
+                value += f"```[{win_str(participantDto['win'])}] {infoDto['gameMode']} | {participantDto['championName']}{pos_str(participantDto['teamPosition'])} | {time_str(infoDto['gameDuration'])}\n"
+                value += f"KDA: {participantDto['kills']}/{participantDto['deaths']}/{participantDto['assists']} ({round(100 * (participantDto['kills'] + participantDto['assists']) / teamDto['objectives']['champion']['kills'])}%) | "
+                value += f"CS: {participantDto['totalMinionsKilled'] + participantDto['neutralMinionsKilled']}({round(60 * (participantDto['totalMinionsKilled'] + participantDto['neutralMinionsKilled']) / infoDto['gameDuration'], 1)}) | "
+                value += f"Gold: {participantDto['goldEarned']}```"
+            
+            embed.add_field(name="Recent Games", value=value, inline=False)
+
+        await ctx.send(embed=embed)
+
+    async def get_summoner(self, ctx: commands.Context, user: str = None) -> tuple[dict, discord.Member] | None:
+        if not user:
+            if (id := str(ctx.author.id)) not in self.db:
+                await error(ctx, "You are not registered.")
+                return
+    
+            if not (summoner := riot.get_summoner_by_puuid(self.db[id]['puuid'])):
+                await error(ctx, "Summoner not found.")
+                return
+            
+            return summoner, ctx.author
+
+        elif re.search(r"^[0-9]+$", user.strip("<@>")):
+            if (id := user.strip("<@>")) not in self.db:
+                await error(ctx, "User is not registered.")
+                return
+            
+            if not (summoner := riot.get_summoner_by_puuid(self.db[id]['puuid'])):
+                await error(ctx, "Summoner not found.")
+                return
+            
+            return summoner, ctx.guild.get_member(int(id))
+            
+        elif len(riotId := user.split("#")) == 2:
+            if not (summoner := riot.get_summoner_by_name(riotId[0], riotId[1])):
+                await error(ctx, "Summoner not found.")
+                return
+            
+            return summoner, None
+
+        else:
+            await error(ctx, "Invalid user.\nUser must be a member or a Riot ID in the form {gameName}#{tagLine}.")
+            return
+
+async def error(ctx: commands.Context, description: str):
+    embed = discord.Embed(title="Woops...", description=description)
+    embed.set_footer(text=ctx.author.display_name, icon_url=ctx.author.display_avatar)
+    await ctx.send(embed=embed)
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(RiotCog(bot))
+    
---
+++ modules/riot_tools.py
@@ -0,0 +1,116 @@
+import os
+from dotenv import load_dotenv
+
+import requests
+import urllib.parse
+
+load_dotenv()
+
+API_KEY = os.getenv("RIOT_KEY")
+
+AMERICA_URL = "https://americas.api.riotgames.com"
+NA_URL = "https://na1.api.riotgames.com"
+DD_URL = "https://ddragon.leagueoflegends.com"
+
+def get_dd_version() -> str | None:
+    '''
+    Returns the latest Data Dragon version.
+    '''
+    resp = requests.get(DD_URL + "/api/versions.json")
+
+    if resp.status_code == 200:
+        return resp.json()[0]
+
+def get_account_by_name(gameName: str, tagLine: str) -> dict | None:
+    gameName = urllib.parse.quote(gameName)
+    tagLine = urllib.parse.quote(tagLine)
+
+    url = AMERICA_URL + f"/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}"
+    resp = requests.get(url + f"?api_key={API_KEY}")
+
+    if resp.status_code == 200:
+        return resp.json()
+
+def get_account_by_puuid(puuid: str) -> dict | None:
+    puuid = urllib.parse.quote(puuid)
+
+    url = AMERICA_URL + f"/riot/account/v1/accounts/by-puuid/{puuid}"
+    resp = requests.get(url + f"?api_key={API_KEY}")
+
+    if resp.status_code == 200:
+        return resp.json()   
+
+def get_summoner_by_name(gameName: str, tagLine: str) -> dict | None:
+    if not (account := get_account_by_name(gameName, tagLine)):
+        return None
+    
+    puuid = urllib.parse.quote(account['puuid'])
+
+    url = NA_URL + f"/lol/summoner/v4/summoners/by-puuid/{puuid}"
+    resp = requests.get(url + f"?api_key={API_KEY}")
+
+    if resp.status_code == 200:
+        summoner = resp.json()
+        summoner.update(account)
+        return summoner
+    
+def get_summoner_by_puuid(puuid: str) -> dict | None:
+    if not (account := get_account_by_puuid(puuid)):
+        return None
+    
+    puuid = urllib.parse.quote(puuid)
+
+    url = NA_URL + f"/lol/summoner/v4/summoners/by-puuid/{puuid}"
+    resp = requests.get(url + f"?api_key={API_KEY}")
+
+    if resp.status_code == 200:
+        summoner = resp.json()
+        summoner.update(account)
+        return summoner
+    
+def get_summoner_icon(iconId: int) -> str | None:
+    '''
+    Returns summoner icon link.
+    '''
+    version = get_dd_version()
+    return DD_URL + f"/cdn/{version}/img/profileicon/{iconId}.png"
+
+def get_stats_by_summoner(summonerId: str) -> dict | None:
+    summonerId = urllib.parse.quote(summonerId)
+
+    url = NA_URL + f"/lol/league/v4/entries/by-summoner/{summonerId}"
+    resp = requests.get(url + f"?api_key={API_KEY}")
+
+    if resp.status_code == 200:
+        return resp.json()
+    
+def get_matchId_by_puuid(puuid: str, count: int = 20) -> list | None:
+    puuid = urllib.parse.quote(puuid)
+
+    url = AMERICA_URL + f"/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count={count}"
+    resp = requests.get(url + f"&api_key={API_KEY}")
+
+    if resp.status_code == 200:
+        return resp.json()
+    
+def get_match_by_matchId(matchId: str) -> dict | None:
+    matchId = urllib.parse.quote(matchId)
+
+    url = AMERICA_URL + f"/lol/match/v5/matches/{matchId}"
+    resp = requests.get(url + f"?api_key={API_KEY}")
+
+    if resp.status_code == 200:
+        return resp.json()
+    
+def get_winrate(puuid: str, matchId: list[str]) -> tuple[int, int] | None:
+    wins = 0
+    losses = 0
+    for match in matchId:
+        if matchDto := get_match_by_matchId(match):
+            participantDto = [participantDto for participantDto in matchDto['info']['participants'] if participantDto['puuid'] == puuid][0]
+            if participantDto['win']:
+                wins += 1
+            else:
+                losses += 1
+
+    return wins, losses
