7d8f8cff8fcfbf4c69662330244f923813521f70
biglavis <105270868+biglavis@users.noreply.github.com>
2024-08-21 16:44:46 -0700

    Added reroll button.

--- cogs/team.py
+++ cogs/team.py
@@ -2,6 +2,25 @@ import random
 import discord
 from discord.ext import commands
 
+class RerollView(discord.ui.View):
+    def __init__(self, participants: list[str], num_teams: int, timeout: int = 180):
+        super().__init__(timeout=timeout)
+        self.participants = participants
+        self.num_teams = num_teams
+
+    @discord.ui.button(label="🎲", style=discord.ButtonStyle.green)
+    async def reroll(self, interaction: discord.Interaction, button: discord.ui.Button):
+        participants = random.sample(self.participants, len(self.participants))
+
+        embed = discord.Embed(title="Teams")
+        for i in range(self.num_teams):
+            value = ""
+            for member in participants[round(len(participants)/self.num_teams*i):round(len(participants)/self.num_teams*(i+1))]:
+                value += f"{member}\n"
+            embed.add_field(name=f"Team {i+1}", value=value)
+
+        await interaction.response.edit_message(embed=embed)
+
 class TeamCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
@@ -39,14 +58,14 @@ class TeamCog(commands.Cog):
         description += "\n`/destroy` to destroy this lobby"
     
         embed = discord.Embed(title="Participants", description=description)
-        await ctx.send(embed=embed, silent=True) 
+        await ctx.send(embed=embed) 
 
     @commands.hybrid_command(brief="Add a participant to the lobby.", description="Add a participant to the lobby.")
     async def add(self, ctx: commands.context, name: str):
         self.participants.append(name)
         embed = discord.Embed(description=f"Added {name} to the lobby")
         embed.set_footer(text="/lobby to see who's in the lobby")
-        await ctx.send(embed=embed, silent=True) 
+        await ctx.send(embed=embed) 
 
     @commands.hybrid_command(brief="Kick a participant from the lobby.", description="Kick a participant from the lobby.")
     async def kick(self, ctx: commands.context, id: int):
@@ -58,7 +77,7 @@ class TeamCog(commands.Cog):
         member = self.participants.pop(id-1)
         embed = discord.Embed(description=f"Kicked {member} from the lobby")
         embed.set_footer(text="/lobby to see who's in the lobby")
-        await ctx.send(embed=embed, silent=True) 
+        await ctx.send(embed=embed) 
 
     @commands.hybrid_command(brief="Create x random teams.", description="Create x random teams.")
     async def teams(self, ctx: commands.context, x: int):
@@ -78,16 +97,14 @@ class TeamCog(commands.Cog):
         
         participants = random.sample(self.participants, len(self.participants))
 
-        embed = embed = discord.Embed(title="Teams")
-
+        embed = discord.Embed(title="Teams")
         for i in range(x):
             value = ""
             for member in participants[round(len(participants)/x*i):round(len(participants)/x*(i+1))]:
                 value += f"{member}\n"
             embed.add_field(name=f"Team {i+1}", value=value)
 
-        message = await ctx.send(embed=embed, silent=True)
-        await message.add_reaction("<:upvote:1275220048616624231")
+        await ctx.send(embed=embed, view=RerollView(participants=participants, num_teams=x))
 
     @commands.hybrid_command(brief="Destroy the current lobby.", description="Destroy the current lobby.")
     async def destroy(self, ctx: commands.context):
